a19879c1c1fb2b47210bfd95aeabfc11a83bc0ef
Kentaro Iwata <kentaro@email.com>
2024-10-31 21:23:40 +0900

    use openssl vendored

--- lib/lambda-event-consumer-stack.ts
+++ lib/lambda-event-consumer-stack.ts
@@ -1,31 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { RustFunction } from 'cargo-lambda-cdk';
+import {Construct} from 'constructs';
+import {RustFunction} from 'cargo-lambda-cdk';
 import {Duration} from "aws-cdk-lib";
-import { Function } from 'aws-cdk-lib/aws-lambda';
+import {Function} from 'aws-cdk-lib/aws-lambda';
+
 export class LambdaEventConsumerStack extends cdk.Stack {
 
-  readonly lambdaFunction: Function;
+    readonly lambdaFunction: Function;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // todo まとめて処理できるようにしたい
-    if (!process.env.LINE_CHANNEL_ACCESS_TOKEN) {
-      throw new Error("LINE_CHANNEL_ACCESS_TOKEN is not set");
+        // todo まとめて処理できるようにしたい
+        if (!process.env.LINE_CHANNEL_ACCESS_TOKEN) {
+            throw new Error("LINE_CHANNEL_ACCESS_TOKEN is not set");
+        }
+
+        this.lambdaFunction = new RustFunction(this, 'event-consumer', {
+            functionName: 'lambda-line-bot-event-consumer-api',
+            description: 'Lambda function for event consumer',
+            memorySize: 128,
+            timeout: Duration.seconds(5),
+            manifestPath: './event_consumer/Cargo.toml',
+            runtime: 'provided.al2023',
+            bundling: {
+                cargoLambdaFlags: [
+                    '--target',
+                    'x86_64-unknown-linux-musl',
+                    '--config',
+                    'rustflags="-C linker=musl-gcc"'
+                ],
+
+            },
+            environment: {
+                'RUST_LOG': 'DEBUG',
+                'LINE_CHANNEL_ACCESS_TOKEN': process.env.LINE_CHANNEL_ACCESS_TOKEN,
+            }
+        });
     }
-
-    this.lambdaFunction = new RustFunction(this, 'event-consumer', {
-      functionName: 'lambda-line-bot-event-consumer-api',
-      description: 'Lambda function for event consumer',
-      memorySize: 128,
-      timeout: Duration.seconds(5),
-      manifestPath: './event_consumer/Cargo.toml',
-      runtime: 'provided.al2023',
-      environment: {
-        'RUST_LOG': 'DEBUG',
-        'LINE_CHANNEL_ACCESS_TOKEN': process.env.LINE_CHANNEL_ACCESS_TOKEN,
-      }
-    });
-  }
 }
